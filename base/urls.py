096f816232a281af29bc88c6e571a0397dab8f72
DELL <sltradition@gmail.com>
2021-09-17 02:25:43 +0530

    ds

---
+++ base/urls.py
@@ -0,0 +1,11 @@
+from django.conf import settings
+from django.conf.urls import url
+from django.contrib import admin
+from django.urls import path
+from django.views.static import serve
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+]
